ae692a341e29a48b3929bee0d219979b5fb863db
CodeKnight314 <rtang24@nmhschool.org>
2024-09-13 14:33:29 +0100

    Add loss.py

---
+++ loss.py
@@ -0,0 +1,78 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+class DiceLoss(nn.Module):
+    def __init__(self, smoothing: float = 1e-6, num_classes: int = 1):
+        super(DiceLoss, self).__init__()
+        self.smoothing = smoothing 
+        self.num_classes = num_classes
+
+    def forward(self, prediction, truth):
+        """
+        prediction: Tensor of shape (batch_size, num_classes, height, width)
+        truth: Tensor of shape (batch_size, num_classes, height, width)
+        """
+        total_loss = 0.0
+        
+        truth_one_hot = F.one_hot(truth, num_classes=self.num_classes)  
+        truth_one_hot = truth_one_hot.permute(0, 3, 1, 2).float()
+
+        for i in range(self.num_classes):
+            prediction_mask_i = prediction[:, i, :, :]
+            truth_mask_i = truth[:, i, :, :]
+
+            prediction_mask_i = prediction_mask_i.contiguous().view(-1)
+            truth_mask_i = truth_mask_i.contiguous().view(-1)
+
+            intersection = (prediction_mask_i * truth_mask_i).sum()
+            dice_coefficient = (2. * intersection + self.smoothing) / (
+                prediction_mask_i.sum() + truth_mask_i.sum() + self.smoothing
+            )
+
+            total_loss += 1 - dice_coefficient
+
+        return total_loss / self.num_classes
+    
+class FocalLoss(nn.Module): 
+    def __init__(self, alpha = None, gamma = 2, reduction : str = "mean"): 
+        super(FocalLoss, self).__init__()
+        
+        self.alpha = alpha 
+        self.gamma = gamma 
+        self.reduction = reduction
+        
+    def forward(self, prediction, truth):
+        """
+        prediction: Tensor of shape (batch_size, num_classes, height, width)
+        truth: Tensor of shape (batch_size, height, width)
+        """
+        N, C, H, W = prediction.shape 
+        
+        prediction = prediction.permute(0, 2, 3, 1).continguous().view(-1, C)
+        truth = truth.view(-1, 1)
+        
+        pt = prediction[range(prediction.size(0)), truth]
+        
+        focal_factor = (1 - pt) ** self.gamma
+
+        if self.alpha is not None:
+            if isinstance(self.alpha, (list, tuple, torch.Tensor)):
+                if not isinstance(self.alpha, torch.Tensor):
+                    self.alpha = torch.tensor(self.alpha, dtype=prediction.dtype, device=prediction.device)
+                alpha_t = self.alpha[truth]
+                loss = -alpha_t * focal_factor * torch.log(pt)
+            else:
+                loss = -focal_factor * torch.log(pt)
+        else:
+            loss = -focal_factor * torch.log(pt)
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        else:
+            return loss
+        
+        
+        
